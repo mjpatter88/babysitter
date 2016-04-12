4f5b0b5d752ed6728e2fc9ee09f79969cd61b081
Michael Patterson <mjpatter88@gmail.com>
2016-04-11 20:40:38 -0500

    GREEN: working from 1am to 2am results in 6.

--- babysitter.py
+++ babysitter.py
@@ -1,7 +1,10 @@
 AFTER_MIDNIGHT_RATE = 16
 
 def calculate(start_time, end_time):
+    if start_time < 5:
+        start_time += 12
     if end_time <= 4:
         end_time += 12
-    work_after_midnight = end_time - 12
+    time_worked = end_time - start_time
+    work_after_midnight = end_time - max(12, start_time)
     return work_after_midnight * AFTER_MIDNIGHT_RATE
