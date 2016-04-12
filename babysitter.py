66e77512f1f77cf81a70d0f480e7cfddb671a57f
Michael Patterson <mjpatter88@gmail.com>
2016-04-12 09:37:05 -0500

    Refactor to clean up making times absolute in this context.

--- babysitter.py
+++ babysitter.py
@@ -1,15 +1,23 @@
 MIDNIGHT = 12
 
+EARLIEST_STARTING_TIME = 5
+EARLIEST_BED_TIME = 5
+LATEST_ENDING_TIME = 4
+
 AFTER_MIDNIGHT_RATE = 16
 BEFORE_BEDTIME_RATE = 12
 
 def calculate(start_time, bed_time, end_time):
-    if start_time < 5:
-        start_time += MIDNIGHT
-    if end_time <= 4:
-        end_time += MIDNIGHT
-    if bed_time < 5:
-        bed_time += MIDNIGHT
+    start_time, bed_time, end_time = _make_times_absolute(start_time, bed_time, end_time)
     work_after_midnight = end_time - max(MIDNIGHT, start_time) if end_time > MIDNIGHT else 0
     work_before_bedtime = min(bed_time, MIDNIGHT) - min(start_time, MIDNIGHT)
     return work_after_midnight * AFTER_MIDNIGHT_RATE + work_before_bedtime * BEFORE_BEDTIME_RATE
+
+def _make_times_absolute(start_time, bed_time, end_time):
+    if start_time < EARLIEST_STARTING_TIME:
+        start_time += MIDNIGHT
+    if bed_time < EARLIEST_BED_TIME:
+        bed_time += MIDNIGHT
+    if end_time <= LATEST_ENDING_TIME:
+        end_time += MIDNIGHT
+    return start_time, bed_time, end_time
