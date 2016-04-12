c21e86d123e227e1258f171a8a323e1e3045047e
Michael Patterson <mjpatter88@gmail.com>
2016-04-12 09:51:05 -0500

    Green: Hours worked before midnight after bedtime are worth .

--- babysitter.py
+++ babysitter.py
@@ -6,12 +6,18 @@ LATEST_ENDING_TIME = 4
 
 AFTER_MIDNIGHT_RATE = 16
 BEFORE_BEDTIME_RATE = 12
+BEDTIME_TO_MIDNIGHT_RATE = 8
 
 def calculate(start_time, bed_time, end_time):
     start_time, bed_time, end_time = _make_times_absolute(start_time, bed_time, end_time)
-    work_after_midnight = end_time - max(MIDNIGHT, start_time) if end_time > MIDNIGHT else 0
+
     work_before_bedtime = min(bed_time, MIDNIGHT) - min(start_time, MIDNIGHT)
-    return work_after_midnight * AFTER_MIDNIGHT_RATE + work_before_bedtime * BEFORE_BEDTIME_RATE
+    work_after_bedtime_before_midnight = min(end_time, MIDNIGHT) - min(bed_time, MIDNIGHT)
+    work_after_midnight = end_time - max(MIDNIGHT, start_time) if end_time > MIDNIGHT else 0
+
+    return work_before_bedtime * BEFORE_BEDTIME_RATE + \
+                work_after_midnight * AFTER_MIDNIGHT_RATE + \
+                work_after_bedtime_before_midnight * BEDTIME_TO_MIDNIGHT_RATE
 
 def _make_times_absolute(start_time, bed_time, end_time):
     if start_time < EARLIEST_STARTING_TIME:
