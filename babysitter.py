4c1fcf0b3a5773caf6a52cb701580f7a3a3a2411
Michael Patterson <mjpatter88@gmail.com>
2016-04-12 09:09:32 -0500

    GREEN: Working one hour with child awake before midnight equals 12.

--- babysitter.py
+++ babysitter.py
@@ -1,10 +1,13 @@
 AFTER_MIDNIGHT_RATE = 16
+BEFORE_BEDTIME_RATE = 12
 
-def calculate(start_time, end_time):
+def calculate(start_time, bed_time, end_time):
     if start_time < 5:
         start_time += 12
     if end_time <= 4:
         end_time += 12
-    time_worked = end_time - start_time
-    work_after_midnight = end_time - max(12, start_time)
-    return work_after_midnight * AFTER_MIDNIGHT_RATE
+    if bed_time < 5:
+        bed_time += 12
+    work_after_midnight = max(12, end_time) - max(12, start_time)
+    work_before_bedtime = min(bed_time, 12) - min(start_time, 12)
+    return work_after_midnight * AFTER_MIDNIGHT_RATE + work_before_bedtime * BEFORE_BEDTIME_RATE
