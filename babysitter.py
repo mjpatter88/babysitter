1b21953ad53ba7ffdec28b1127263ed44e3d4eb1
Michael Patterson <mjpatter88@gmail.com>
2016-04-12 09:27:29 -0500

    Refactor to make work after midnight logic a little more clear.

--- babysitter.py
+++ babysitter.py
@@ -10,6 +10,6 @@ def calculate(start_time, bed_time, end_time):
         end_time += MIDNIGHT
     if bed_time < 5:
         bed_time += MIDNIGHT
-    work_after_midnight = max(MIDNIGHT, end_time) - max(MIDNIGHT, start_time)
+    work_after_midnight = end_time - max(MIDNIGHT, start_time) if end_time > MIDNIGHT else 0
     work_before_bedtime = min(bed_time, MIDNIGHT) - min(start_time, MIDNIGHT)
     return work_after_midnight * AFTER_MIDNIGHT_RATE + work_before_bedtime * BEFORE_BEDTIME_RATE
