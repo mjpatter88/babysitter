1436bdcf157e98609e8d6c894cb4e3438cfa6d67
Michael Patterson <mjpatter88@gmail.com>
2016-04-11 20:28:32 -0500

    RED: test that working from midnight to 4am results in 4.

--- babysitter_test.py
+++ babysitter_test.py
@@ -10,6 +10,9 @@ class TestBabysitter(unittest.TestCase):
         total = babysitter.calculate(12, 12)
         self.assertEquals(total, 0, "Calculate did not return 0 when no time was worked.")
 
+    def test__calculate_returns_sixty_four_for_working_midnight_to_four(self):
+        total = babysitter.calculate(12, 4)
+        self.assertEquals(total, 64, "Working midnight to 4am should be $64.")
 
 if __name__ == "__main__":
     unittest.main()
