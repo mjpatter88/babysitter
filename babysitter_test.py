2bf470565af7b6066f8ad67894eb019f3c812187
Michael Patterson <mjpatter88@gmail.com>
2016-04-12 09:09:09 -0500

    RED: Test that working one hour with child awake before midnight equals 12.

--- babysitter_test.py
+++ babysitter_test.py
@@ -7,17 +7,20 @@ class TestBabysitter(unittest.TestCase):
         pass
 
     def test__calculate_returns_zero_if_no_time_is_worked(self):
-        total = babysitter.calculate(12, 12)
+        total = babysitter.calculate(12, 12, 12)
         self.assertEquals(total, 0, "Calculate did not return 0 when no time was worked.")
 
     def test__calculate_returns_sixty_four_for_working_midnight_to_four(self):
-        total = babysitter.calculate(12, 4)
+        total = babysitter.calculate(12, 12, 4)
         self.assertEquals(total, 64, "Working midnight to 4am should be $64.")
 
     def test__calculate_returns_sixteen_for_working_1_to_2(self):
-        total = babysitter.calculate(1, 2)
+        total = babysitter.calculate(1, 2, 2)
         self.assertEquals(total, 16, "Working 1am to 2am should be $16.")
 
+    def test__calculate_returns_twelve_for_working_nine_to_ten_with_bedtime_at_ten(self):
+        total = babysitter.calculate(9, 10, 10)
+        self.assertEquals(total, 12, "Working one hour with kid awake should be $12.")
 
 if __name__ == "__main__":
     unittest.main()
