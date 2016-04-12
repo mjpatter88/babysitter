99ed48f1f484fe5a0e07db87b974a8960bf7d371
Michael Patterson <mjpatter88@gmail.com>
2016-04-12 09:11:54 -0500

    Refactor to make midnight a constant.

--- babysitter.py
+++ babysitter.py
@@ -1,13 +1,15 @@
+MIDNIGHT = 12
+
 AFTER_MIDNIGHT_RATE = 16
 BEFORE_BEDTIME_RATE = 12
 
 def calculate(start_time, bed_time, end_time):
     if start_time < 5:
-        start_time += 12
+        start_time += MIDNIGHT
     if end_time <= 4:
-        end_time += 12
+        end_time += MIDNIGHT
     if bed_time < 5:
-        bed_time += 12
-    work_after_midnight = max(12, end_time) - max(12, start_time)
-    work_before_bedtime = min(bed_time, 12) - min(start_time, 12)
+        bed_time += MIDNIGHT
+    work_after_midnight = max(MIDNIGHT, end_time) - max(MIDNIGHT, start_time)
+    work_before_bedtime = min(bed_time, MIDNIGHT) - min(start_time, MIDNIGHT)
     return work_after_midnight * AFTER_MIDNIGHT_RATE + work_before_bedtime * BEFORE_BEDTIME_RATE
