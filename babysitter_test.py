a4bd226e4a182767bfa7b373476902c56e2a058b
Michael Patterson <mjpatter88@gmail.com>
2016-04-11 20:21:11 -0500

    RED: test that working no time results in no wages.

--- babysitter_test.py
+++ babysitter_test.py
@@ -5,8 +5,9 @@ class TestBabysitter(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test__first_failing_test(self):
-        self.assertEquals(True, False, "first test")
+    def test__calculate_returns_zero_if_no_time_is_worked(self):
+        total = babysitter.calculate(12, 12)
+        self.assertEquals(total, 0, "Calculate did not return 0 when no time was worked.")
 
 
 
