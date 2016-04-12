8e964451ca411ad495a2c1948197b1ce70e30d2a
Michael Patterson <mjpatter88@gmail.com>
2016-04-11 20:35:20 -0500

    GREEN: test that working from midnight to 4am results in 4.

--- babysitter.py
+++ babysitter.py
@@ -1,3 +1,7 @@
+AFTER_MIDNIGHT_RATE = 16
 
 def calculate(start_time, end_time):
-    return 0
+    if end_time <= 4:
+        end_time += 12
+    work_after_midnight = end_time - 12
+    return work_after_midnight * AFTER_MIDNIGHT_RATE
